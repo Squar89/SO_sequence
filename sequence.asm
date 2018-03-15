50b51c9cd502a5e6870da0a74c3d460cd6be76a9
Squar <squar89@gmail.com>
2018-03-15 18:01:42 +0100

    Implemented handling arguments and opening given file

--- sequence.asm
+++ sequence.asm
@@ -5,17 +5,49 @@ section .data
   SYS_WRITE   equ 1
   SYS_OPEN    equ 2
   SYS_CLOSE   equ 3
+  ARG_NUM     equ 2;number of arguments
+  O_RDONLY    equ 0
+
 
 section .bss
+  fd          resb 8
 
 section .text
   global _start
 
 _start:
-  mov rax, SYS_EXIT
-  mov rbx, 0
-  syscall
-  
-_exit:
+  ;check if program was executed with correct number of arguments
+  pop   rbx
+  cmp   rbx, ARG_NUM
+  jne   _exit_error
 
-_error:
+  ;open file given as first argument
+  pop   rax   ;skip "./sequence"
+  pop   rdi   ;get input file
+  mov   rax, SYS_OPEN
+  mov   rsi, O_RDONLY
+  mov   rdx, 0 ;no additional flags for sys_open
+  syscall
+
+  ;check if opening input file was successful
+  cmp   rax, 0
+  jl    _exit_error
+
+  mov   [fd], rax ;store file descriptor
+
+
+
+
+
+;program completed succesfully, given sequence is correct
+_exit_success:
+  mov   rax, SYS_EXIT
+  mov   rdi, 0
+  syscall
+
+;program encountered an error, either it was executed with
+;wrong parameters or the given sequence is not correct
+_exit_error:
+  mov   rax, SYS_EXIT
+  mov   rdi, 1
+  syscall
